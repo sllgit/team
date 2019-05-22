3444ad5da44a35adefb80b0fdae413c0dfcffd76
sll <1477380279@qq.com>
2019-05-22 11:20:10 +0800

    new team

---
+++ team/app/Http/Controllers/admin/IndexController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    /**
+     * @content 后台首页
+     */
+    public function index()
+    {
+        return view('admin.indexx');
+    }
+
+    /**
+     * @content 后台退出登录
+     */
+    public function editlogin()
+    {
+        return view('admin.login');
+    }
+}
